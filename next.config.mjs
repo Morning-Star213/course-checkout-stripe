e1f9a54f1b45a789b0fb31da394a41ba36836924
simba0808 <simba960808@gmail.com>
2024-08-01 22:01:10 +1000

    remove signup

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  redirects: async () => [
-    {
-      source: '/',
-      destination: '/signup?step=1',
-      permanent: true,
-    },
-  ]
 };
 
 export default nextConfig;
